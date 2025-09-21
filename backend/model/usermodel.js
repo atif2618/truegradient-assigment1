69cd2fe8a97073b78567fe0424f0799cec2726cc
atif2618 <151423780+atif2618@users.noreply.github.com>
2025-09-21 20:06:43 +0530

    Update usermodel.js

--- backend/model/usermodel.js
+++ backend/model/usermodel.js
@@ -26,14 +26,7 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
   credits: { type: Number, default: 10 },
-  notifications: [
-    {
-      title: String,
-      body: String,
-      read: { type: Boolean, default: false },
-      createdAt: { type: Date, default: Date.now }
-    }
-  ]
 });
 
 module.exports = mongoose.model('User', UserSchema);
+
